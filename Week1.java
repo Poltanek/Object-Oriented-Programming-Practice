c12bc00a9828f8c7f4f54c672f41f17c9b75e2f5
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2025-01-27 14:59:39 +0000

    ver1

---
+++ Week1.java
@@ -0,0 +1,31 @@
+public class Week1 {
+    public static void main(String[] args) {
+        int b = 10;
+        int a = 12;
+        int c = 10;
+
+        System.out.println("a==b " + (a==b));
+        System.out.println("a==c " + (a==c));
+        System.out.println("b==c " + (b==c));
+        System.out.println("a!=b " + (a!=b));
+        System.out.println("c!=b " + (c!=b));
+
+        System.out.println("a>b " + (a>b));
+        System.out.println("c>b " + (c>b));
+        System.out.println("a>=b " + (a>=b));
+        System.out.println("c>=b " + (c>=b));
+        System.out.println("a<b " + (a<b));
+        System.out.println("c<b " + (c<b));
+        System.out.println("a<=b " + (a<=b));
+        System.out.println("c<=b " + (c<=b));
+
+        /* If the 'myString variable is declared and if the print statement
+        * Then the
+        *
+        */
+        String myString = "abc";
+        System.out.println(myString.equals("abc"));
+        System.out.println(myString.equals("awdawd"));
+
+    }
+}
