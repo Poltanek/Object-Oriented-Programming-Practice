6b61fe69a69c60e4275355f46cff236c95153bd6
Adam Tanweer <113802807+Poltanek@users.noreply.github.com>
2025-02-14 22:19:56 +0000

    update

---
+++ src/Week1/Cat/Cat.java
@@ -0,0 +1,62 @@
+public class Cat {
+    private String name;
+    private String weight;
+    private String breed;
+    private String model;
+    private String colour;
+
+    public Cat(String name, String weight, String breed, String model, String colour) {
+        this.name = name;
+        this.weight = weight;
+        this.breed = breed;
+        this.model = model;
+        this.colour = colour;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getWeight() {
+        return weight;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getColour() {
+        return colour;
+    }
+
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void play() {
+        System.out.println("Playing");
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
